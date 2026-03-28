3b34008ee76c2790ef141d79349e1c260da4e604
mdnabeel315 <md.nabeel315@gmail.com>
2026-03-29 01:02:09 +0530

    Delete zna-streamlit-cloud/zna-repo directory

--- zna-streamlit-cloud/zna-repo/backend/__init__.py
+++
@@ -1,5 +0,0 @@
-from .llm_service        import complete, LLMError, LLMParseError
-from .resume_service     import parse_raw_data, generate_resume, parse_and_generate, TEMPLATE_STYLES
-from .ats_service        import run_ats_scan, extract_keywords, score_label
-from .cover_letter_service import generate_cover_letter, TONES
-from .pdf_service        import generate_resume_pdf, generate_cover_letter_pdf
